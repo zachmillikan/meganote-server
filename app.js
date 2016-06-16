bfa20d45b744629ef184318c0fdd275df0490412
Zach <millikanzach@gmail.com>
2016-06-16 15:23:55 -0400

    Added Schema to display the database

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 require('dotenv').load();
 var express = require('express');
+var db = require('mongoose');
+
+db.connect(process.env.DB_URI);
 var app = express();
 
+var NoteSchema = db.Schema({
+  title: String,
+  body_html: String,
+  body_text: String,
+  updated_at: { type: Date, default: Date.now }
+});
+
+var Note = db.model('Note', NoteSchema);
+
 app.get('/', function(req, res) {
-  res.json([
-    {
-      title: 'My hardcoded note.',
-      body_html: 'wow'
-    },
-    {
-      title: 'Another hardcoded note.',
-      body_html: 'Such JSON.'
-    }
-  ]);
+  Note
+    .find()
+    .then(function(notes) {
+      res.json(notes);
+    });
 });
 
 app.listen(3030, function() {
