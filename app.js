dcde0288b47b4c892c8d7bb7e4cc9d2cd1ebb35a
Zach <millikanzach@gmail.com>
2016-06-16 14:18:12 -0400

    Hardcoded array

--- app.js
+++ app.js
@@ -2,7 +2,16 @@ var express = require('express');
 var app = express();
 
 app.get('/', function(req, res) {
-  res.send('I <3 Express');
+  res.json([
+    {
+      title: 'My hardcoded note.',
+      body_html: 'wow'
+    },
+    {
+      title: 'Another hardcoded note.',
+      body_html: 'Such JSON.'
+    }
+  ]);
 });
 
 app.listen(3030, function() {
