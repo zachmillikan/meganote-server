929f282833e2908cba14882e64e667805f2088c6
Zach <millikanzach@gmail.com>
2016-06-16 14:12:29 -0400

    Send a plain text

--- app.js
+++ app.js
@@ -1,6 +1,10 @@
 var express = require('express');
 var app = express();
 
+app.get('/', function(req, res) {
+  res.send('I <3 Express');
+});
+
 app.listen(3030, function() {
   console.log('Listening on http://localhost:3030...');
 });
