c56d3ec2f575de274ff6261c0c09f53f81bf1af4
Zach <millikanzach@gmail.com>
2016-06-17 13:41:58 -0400

    Sort notes on the server

--- app.js
+++ app.js
@@ -2,21 +2,42 @@ require('dotenv').load();
 var express = require('express');
 var db = require('./config/db');
 var Note = require('./models/note');
+var bodyParser = require('body-parser');
 
 var app = express();
 
 app.use(function(req, res, next) {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', "POST, GET, DELETE, PUT");
-  res.header('Access-Control-Allow-Headers', "Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
+  // res.header('Access-Control-Allow-Methods', "POST, GET, DELETE, PUT");
+  res.header('Access-Control-Allow-Headers',
+    'Content-Type');
   next();
 });
+
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   Note
     .find()
+    .sort({ title: 'asc'})
     .then(function(notes) {
       res.json(notes);
+    });
+});
+
+app.post('/', function(req, res) {
+  var note = new Note({
+    title: req.body.note.title,
+    body_html: req.body.note.body_html
+  });
+
+  note
+    .save()
+    .then(function(noteData) {
+      res.json({
+        message: 'Successfully created note',
+        note: noteData
+      });
     });
 });
 
