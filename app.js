fa0f69ccff072239d30a28620f696aaf3bbc719d
Zach <millikanzach@gmail.com>
2016-07-08 09:26:47 -0400

    Made changes to the the route of the server

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 require('dotenv').load();
 var express = require('express');
-var db = require('./config/db');
-var Note = require('./models/note');
 var bodyParser = require('body-parser');
+var noteRoutes = require('./routes/note-routes');
 
 var app = express();
 
 app.use(function(req, res, next) {
+
   res.header('Access-Control-Allow-Origin', '*');
   // res.header('Access-Control-Allow-Methods', "POST, GET, DELETE, PUT");
   res.header('Access-Control-Allow-Headers',
@@ -20,82 +20,8 @@ app.use(function(req, res, next) {
 
 app.use(bodyParser.json());
 
-app.get('/', function(req, res) {
-  Note
-    .find()
-    .sort({ updated_at: 'desc'})
-    .then(function(notes) {
-      res.json(notes);
-    });
-});
-
-//READ one note
-app.get('/:id', function(req, res) {
-  Note
-    .findOne({
-      _id: req.params.id
-    })
-    .then(function(note) {
-      res.json(note);
-    });
-});
-
-// CREATE a note
-app.post('/', function(req, res) {
-  var note = new Note({
-    title: req.body.note.title,
-    body_html: req.body.note.body_html
-  });
-
-  note
-    .save()
-    .then(function(noteData) {
-      res.json({
-        message: 'Successfully created note',
-        note: noteData
-      });
-    });
-});
-
-//UPDATE a note
-app.put('/:id', function(req, res) {
-  Note
-    .findOne({
-      _id: req.params.id
-    })
-    .then(function(note) {
-      note.title = req.body.note.title;
-      note.body_html = req.body.note.body_html;
-      note
-        .save()
-        .then(function() {
-          res.json({
-              message: 'Your changes have been saved.',
-              note: note
-            });
-        });
-      },
-      function(result) {
-        res.json({ message: 'Aww, cuss!' });
-    });
-});
-
-app.delete('/:id', function(req, res) {
-  Note
-  .findOne({
-    _id: req.params.id
-  })
-  .then(function(note) {
-    note
-    .remove()
-    .then(function() {
-      res.json({
-        message: 'Note deleted',
-        note: note
-      })
-    })
-  });
-});
+//Routes
+app.use('/api/v1/notes', noteRoutes));
 
 //Create a user
 app.post('/users', function(req, res) {
---
+++ routes/note-routes.js
@@ -0,0 +1,81 @@
+var router = require('express').Router();
+var Note = require('../models/note');
+//READ all notes
+router.get('/', function(req, res) {
+  Note
+    .find()
+    .sort({ updated_at: 'desc'})
+    .then(function(notes) {
+      res.json(notes);
+    });
+});
+
+//READ one note
+router.get('/:id', function(req, res) {
+  Note
+    .findOne({
+      _id: req.params.id
+    })
+    .then(function(note) {
+      res.json(note);
+    });
+});
+
+// CREATE a note
+router.post('/', function(req, res) {
+  var note = new Note({
+    title: req.body.note.title,
+    body_html: req.body.note.body_html
+  });
+
+  note
+    .save()
+    .then(function(noteData) {
+      res.json({
+        message: 'Successfully created note',
+        note: noteData
+      });
+    });
+});
+
+//UPDATE a note
+router.put('/:id', function(req, res) {
+  Note
+    .findOne({
+      _id: req.params.id
+    })
+    .then(function(note) {
+      note.title = req.body.note.title;
+      note.body_html = req.body.note.body_html;
+      note
+        .save()
+        .then(function() {
+          res.json({
+              message: 'Your changes have been saved.',
+              note: note
+            });
+        });
+      },
+      function(result) {
+        res.json({ message: 'Aww, cuss!' });
+    });
+});
+
+router.delete('/:id', function(req, res) {
+  Note
+  .findOne({
+    _id: req.params.id
+  })
+  .then(function(note) {
+    note
+    .remove()
+    .then(function() {
+      res.json({
+        message: 'Note deleted',
+        note: note
+      })
+    })
+  });
+});
+
+module.exports = router; 
