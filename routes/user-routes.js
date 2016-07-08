bb5306578ceeb6cdae72d4fa88565e869fc7320f
Zach <millikanzach@gmail.com>
2016-07-08 10:19:15 -0400

    Verify that password and passwordConfirmation match

--- routes/user-routes.js
+++ routes/user-routes.js
@@ -3,6 +3,11 @@ var User = require('../models/user');
 
 //Create a user
 app.post('/', function(req, res) {
+  if (!passwordsPresent(req.body.user) || !passwordsMatch(req.body.user)) {
+    res.status(418).json({
+      message: 'Passwords must match!'
+    });
+  }
   var user = new User({
     name: req.body.user.name,
     username: req.body.user.username,
@@ -16,3 +21,11 @@ app.post('/', function(req, res) {
 });
 
 module.exports = router;
+
+function passwordsPresent(payload) {
+  return (payload.password && payload.passwordConfirmation)
+}
+
+function passwordsMatch(payload) {
+  return (payload.password === payload.passwordConfirmation)
+}
